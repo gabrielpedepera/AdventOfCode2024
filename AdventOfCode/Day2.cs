7ba525ad244cb4dcefa12ac66f4839090bf72d51
Gabriel Pereira <gabrielpedepera@gmail.com>
2024-12-02 22:44:56 +0000

    Resolve Day 2, Part 1

--- AdventOfCode/Day2.cs
+++ AdventOfCode/Day2.cs
@@ -15,11 +15,40 @@ public class Day_2 : BaseDay
 
     private string SolvePart1()
     {
-        return "Not implemented";
+        var lines = _input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+        var reports = lines
+                .Select(line =>
+                    line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray()
+                ).ToArray();
+
+        int safeReportCount = reports.Count(IsSafeReport);
+        return safeReportCount.ToString();
     }
 
     private string SolvePart2()
     {
         return "Not implemented";
+    }
+
+    private bool IsSafeReport(int[] report)
+    {
+        bool isIncreasing = report[1] > report[0];
+        for (int i = 1; i < report.Length; i++)
+        {
+            int diff = report[i] - report[i - 1];
+            if (Math.Abs(diff) < 1 || Math.Abs(diff) > 3)
+            {
+                return false; // Adjacent levels differ by less than 1 or more than 3
+            }
+            if ((isIncreasing && report[i] < report[i - 1]) || (!isIncreasing && report[i] > report[i - 1]))
+            {
+                return false; // Levels are not consistently increasing or decreasing
+            }
+        }
+
+        return true;
     }
 }
