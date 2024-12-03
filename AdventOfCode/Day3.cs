99f4e3ed1d9c7af4af6dfef217fadcd8f427a82a
Gabriel Pereira <gabrielpedepera@gmail.com>
2024-12-03 20:10:16 +0000

    Resolve Day 3, Part 2

--- AdventOfCode/Day3.cs
+++ AdventOfCode/Day3.cs
@@ -22,6 +22,13 @@ public class Day3 : BaseDay
         return result.ToString();
     }
 
+    private string SolvePart2()
+    {
+        var matches = ExtractMulPatternsWithControl(_input);
+        var result = MultiplyAndSum(matches);
+        return result.ToString();
+    }
+
     private List<(int, int)> ExtractMulPatterns(string input)
     {
         var pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
@@ -38,12 +45,37 @@ public class Day3 : BaseDay
         return result;
     }
 
+    private List<(int, int)> ExtractMulPatternsWithControl(string input)
+    {
+        var pattern = @"do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)";
+
+        var matches = Regex.Matches(input, pattern);
+        var result = new List<(int, int)>();
+        bool isEnabled = true;
+
+        foreach (Match match in matches)
+        {
+            if (match.Value == "do()")
+            {
+                isEnabled = true;
+            }
+            else if (match.Value == "don't()")
+            {
+                isEnabled = false;
+            }
+            else if (isEnabled && match.Groups.Count == 3)
+            {
+                int x = int.Parse(match.Groups[1].Value);
+                int y = int.Parse(match.Groups[2].Value);
+                result.Add((x, y));
+            }
+        }
+
+        return result;
+    }
+
     private int MultiplyAndSum(List<(int, int)> pairs)
     {
         return pairs.Sum(pair => pair.Item1 * pair.Item2);
-    }
-    private string SolvePart2()
-    {
-        return "Not implemented";
     }
 }
