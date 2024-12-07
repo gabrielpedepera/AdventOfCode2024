733d2642e2231c75d93ae46c3ce48ccc23a53d1d
Gabriel Pereira <gabrielpedepera@gmail.com>
2024-12-07 19:30:58 +0000

    Resolve Day 5, Part 2

--- AdventOfCode/Day5.cs
+++ AdventOfCode/Day5.cs
@@ -60,6 +60,50 @@ public class Day5 : BaseDay
 
     private string SolvePart2()
     {
-        return "Not implemented";
+        var sections = _input.Split(new[] { "\n\n" }, StringSplitOptions.RemoveEmptyEntries);
+        var rules = sections[0].Split('\n').Select(line => line.Split('|').Select(int.Parse).ToArray()).ToArray();
+        var updates = sections[1].Split('\n').Select(line => line.Split(',').Select(int.Parse).ToArray()).ToArray();
+
+        var ruleDict = new Dictionary<int, HashSet<int>>();
+        foreach (var rule in rules)
+        {
+            if (!ruleDict.ContainsKey(rule[0]))
+            {
+                ruleDict[rule[0]] = new HashSet<int>();
+            }
+            ruleDict[rule[0]].Add(rule[1]);
+        }
+
+        int sumOfMiddlePages = 0;
+
+        foreach (var update in updates)
+        {
+            if (!IsCorrectlyOrdered(update, ruleDict))
+            {
+                var sortedUpdate = SortUpdate(update, ruleDict);
+                int middleIndex = sortedUpdate.Length / 2;
+                sumOfMiddlePages += sortedUpdate[middleIndex];
+            }
+        }
+
+        return sumOfMiddlePages.ToString();
+    }
+
+    private int[] SortUpdate(int[] update, Dictionary<int, HashSet<int>> ruleDict)
+    {
+        var sortedUpdate = update.ToList();
+        sortedUpdate.Sort((a, b) =>
+        {
+            if (ruleDict.ContainsKey(a) && ruleDict[a].Contains(b))
+            {
+                return -1;
+            }
+            if (ruleDict.ContainsKey(b) && ruleDict[b].Contains(a))
+            {
+                return 1;
+            }
+            return 0;
+        });
+        return sortedUpdate.ToArray();
     }
 }
